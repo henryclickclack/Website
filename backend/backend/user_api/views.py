79b570290f34bdad2e7a2e4715a32cac8139ccb9
Henry Hommel <69155948+henryclickclack@users.noreply.github.com>
2023-06-26 19:00:11 +0200

    Add files via upload

---
+++ backend/backend/user_api/views.py
@@ -0,0 +1,54 @@
+from django.contrib.auth import get_user_model, login, logout
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserRegisterSerializer, UserLoginSerializer, UserSerializer
+from rest_framework import permissions, status
+from .validations import custom_validation, validate_email, validate_password
+from user_api.models import ApiKeys
+
+class UserRegister(APIView):
+	permission_classes = (permissions.AllowAny,)
+	def post(self, request):
+		clean_data = custom_validation(request.data)
+		serializer = UserRegisterSerializer(data=clean_data)
+		if serializer.is_valid(raise_exception=True):
+			user = serializer.create(clean_data)
+			if user:
+				return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLogin(APIView):
+	permission_classes = (permissions.AllowAny,)
+	authentication_classes = (SessionAuthentication,)
+	##
+	def post(self, request):
+		data = request.data
+		assert validate_email(data)
+		assert validate_password(data)
+		serializer = UserLoginSerializer(data=data)
+		if serializer.is_valid(raise_exception=True):
+			user = serializer.check_user(data)
+			login(request, user)
+			return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UserLogout(APIView):
+	permission_classes = (permissions.AllowAny,)
+	authentication_classes = ()
+	def post(self, request):
+		logout(request)
+		return Response(status=status.HTTP_200_OK)
+
+
+class UserView(APIView):
+	permission_classes = (permissions.IsAuthenticated,)
+	authentication_classes = (SessionAuthentication,)
+	def get(self, request):
+		serializer = UserSerializer(request.user)
+		print(serializer.data["user_id"])
+		res = ApiKeys.objects.filter(user_id=serializer.data["user_id"])
+		inf = [{"id":x.id,"user_id":x.user_id,"api_key":x.api_key} for x in list(res)]
+		print(inf)
+		return Response({'user': serializer.data,'api_keys':[x["api_key"][:10]+ "......................" for x in inf]}, status=status.HTTP_200_OK)
